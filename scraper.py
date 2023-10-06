39ce259dfaf546132797b058daf59bbe40de54a2
Dylan <dylan@tritan.dev>
2023-10-05 20:00:17 -0400

    cleaned up a bit

--- scraper.py
+++ scraper.py
@@ -1,50 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
 import json
 
 debug = False
 
-
-def main():
-    # Does the user want to convert .json to .csv, or lookup an ASN?
+def get_user_choice():
     print("1. Convert .json to .csv (peers or prefixes)")
     print("2. Lookup an ASN (peers or prefixes)")
     print("3. Lookup prefixes by ASN dump (json peers file)")
-    choice = input("Enter your choice: ").strip()
+    return input("Enter your choice: ").strip()
+
+def read_json_file(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+def write_csv_file(output_filename, data, headers):
+    with open(output_filename, 'w') as f:
+        f.write(",".join(headers) + "\n")
+        for item in data:
+            f.write(",".join([str(item[field]) for field in headers]) + "\n")
+
+def main():
+    choice = get_user_choice()
 
     if choice == "1":
-        # Get the filename
         filename = input("Enter the filename: ").strip()
-
-        # Open the file
-        with open(filename, 'r') as f:
-            data = json.load(f)
-
-        # Get the output filename
+        data = read_json_file(filename)
         output_filename = input("Enter the output filename: ").strip()
 
-        # Open the output file
-        with open(output_filename, 'w') as f:
-            # Is it peers or prefixes?
-            if "asn" in data[0]:
-                # Write the header
-                f.write("Flag,ASN,ASN Name\n")
+        if "asn" in data[0]:
+            headers = ["Flag", "ASN", "ASN Name"]
+            write_csv_file(output_filename, data, headers)
 
-                # Iterate through the data
-                for item in data:
-                    # Write the data to the file
-                    f.write(
-                        f'{item["flag"]},{item["asn"]},{item["asn_name"]}\n')
-            if "prefix" in data[0]:
-                # Write the header
-                f.write("Prefix\n")
-
-                # Iterate through the data
-                for item in data:
-                    # Write the data to the file
-                    f.write(f'{item["prefix"]}\n')
+        if "prefix" in data[0]:
+            headers = ["Prefix"]
+            write_csv_file(output_filename, data, headers)
 
     if choice == "2":
         options = Options()
@@ -59,129 +50,60 @@ def main():
         if debug:
             options.add_argument("--remote-debugging-port=9222")
 
-        driver = webdriver.Chrome(
-            options=options)
-
-        # Get le user input (which asn they wanna see??)
+        driver = webdriver.Chrome(options=options)
         asn = input("Enter the ASN: ").strip()
 
-        # validate:(
         if not asn.isdigit():
             print("Invalid ASN.")
             exit()
 
+        shouldSort = input("Sort by country? (y/n): ").strip().lower() == "y"
+        whatSort = input("What country? (e.g. US): ").strip() if shouldSort else None
+
+        shouldDump = input("Dump results to .json file? (y/n): ").strip().lower() == "y"
         dumps = []
-        shouldDump = False
-        shouldSort = False
-        whatSort = ""
 
-        # Should we sort by flag?
-        sort = input("Sort by country? (y/n): ").strip()
-
-        if sort.lower() == "y":
-            shouldSort = True
-
-            # Ask the user what country they want to sort by
-            whatSort = input("What country? (e.g. US): ").strip()
-
-        # Ask the user if they want to dump the results to a .json file
-        dump = input("Dump results to .json file? (y/n): ")
-
-        if dump.lower() == "y":
-            shouldDump = True
-
-        # Does the user want to see their peers or their prefixes?
         print("\n1. Peers")
         print("2. Prefixes")
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            # Navigate to the URL
             url = f'https://bgp.tools/as/{asn}#connectivity'
             driver.get(url)
 
-            # Find the tbody element using XPath
             xpath = '/html/body/div[1]/div[5]/div[2]/div[1]/div[7]/table[3]/tbody'
             tbody = driver.find_element(By.XPATH, xpath)
 
-            # Check if tbody is found
             if tbody is not None:
-                # Iterate through each tr inside tbody
                 for tr in tbody.find_elements(By.TAG_NAME, 'tr'):
-                    flag = ""
-                    asn = ""
-                    asn_name = ""
-
+                    flag, asn, asn_name = "", "", ""
                     trs = tr.find_elements(By.TAG_NAME, 'td')
+                    img = trs[0].find_element(By.TAG_NAME, 'img')
+                    flag = img.get_attribute('title')
+                    asn = trs[1].get_attribute('data-sort')
+                    asn_name = trs[2].text
 
-                    # Find the td element inside each tr
-                    td1 = trs[0]
-                    if td1 is not None:
-                        # Get the image element inside the td
-                        img = td1.find_element(By.TAG_NAME, 'img')
+                    if shouldDump and (not shouldSort or flag == whatSort):
+                        dumps.append({"flag": flag, "asn": asn, "asn_name": asn_name})
 
-                        # Set the flag
-                        flag = img.get_attribute('title')
-
-                    # Remove the first td element
-                    as_td = trs[1]
-                    if as_td is not None:
-                        # ASN is stored inside attribute "data-sort"
-                        asn = as_td.get_attribute('data-sort')
-
-                    # Get the asn_name
-                    asn_name_td = trs[2]
-                    if asn_name_td is not None:
-                        asn_name = asn_name_td.text
-
-                    # Append the data to the list
-                    if shouldDump:
-                        if shouldSort:
-                            if flag == whatSort:
-                                dumps.append({
-                                    "flag": flag,
-                                    "asn": asn,
-                                    "asn_name": asn_name
-                                })
-                        else:  # dont sort
-                            dumps.append({
-                                "flag": flag,
-                                "asn": asn,
-                                "asn_name": asn_name
-                            })
-
-                    # Print the data
-                    if shouldSort:
-                        if flag == whatSort:
-                            print(f'{flag} {asn} {asn_name}')
-                    else:  # dont sort
+                    if not shouldSort or flag == whatSort:
                         print(f'{flag} {asn} {asn_name}')
 
-            else:
-                print("Tbody not found.")
-
-            if shouldDump:
-                with open('peers.json', 'w') as f:
-                    json.dump(dumps, f)
+                if shouldDump:
+                    with open('peers.json', 'w') as f:
+                        json.dump(dumps, f)
 
         if choice == "2":
-            # Navigate to the URL
             url = f'https://bgp.tools/as/{asn}#prefixes'
             driver.get(url)
-
-            # im sorry : (
             css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
             elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
 
-            # Iterate through the elements to get the text content
-            for element in elements:
-                prefix_text = element.text
-                print(prefix_text)
+            dumps = [{"prefix": element.text} for element in elements] if shouldDump else None
+            prefixes = [element.text for element in elements]
 
-                if shouldDump:
-                    dumps.append({
-                        "prefix": prefix_text
-                    })
+            for prefix in prefixes:
+                print(prefix)
 
             if shouldDump:
                 with open('prefixes.json', 'w') as f:
@@ -200,45 +122,22 @@ def main():
         if debug:
             options.add_argument("--remote-debugging-port=9222")
 
-        driver = webdriver.Chrome(
-            options=options)
-
-        # Get le user input (which asn they wanna see??)
+        driver = webdriver.Chrome(options=options)
         filename = input("Enter the filename: ").strip()
-
-        # Open the file
-        with open(filename, 'r') as f:
-            data = json.load(f)
-
-        # Get the output filename
+        data = read_json_file(filename)
         output_filename = input("Enter the output filename: ").strip()
 
-        # Open the output file
         with open(output_filename, 'w') as f:
-            # Write the header
             f.write("Prefix\n")
-
-            # Iterate through the data
             for item in data:
-                # Navigate to the URL
                 url = f'https://bgp.tools/as/{item["asn"]}#prefixes'
                 driver.get(url)
-
-                # im sorry : (
                 css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
                 elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
+                prefixes = [element.text for element in elements]
+                f.write("\n".join(prefixes) + "\n")
 
-                # Iterate through the elements to get the text content
-                for element in elements:
-                    prefix_text = element.text
-                    print(prefix_text)
-
-                    # Write the data to the file
-                    f.write(f'{prefix_text}\n')
-
-    # Close the browser
-    driver.quit()
-
+        driver.quit()
 
 if __name__ == "__main__":
     try:
