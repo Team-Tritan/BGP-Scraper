d5e83ab9f64372544e24d26728091d527d05daff
Dylan <dylan@tritan.dev>
2023-10-05 22:07:49 -0400

    restructured a bit, fixed peer ip lookup from dumps

--- scraper.py
+++ scraper.py
@@ -7,9 +7,9 @@ debug = False
 
 # Function to get user's choice
 def get_user_choice():
-    print("1. Lookup an ASN (peers or prefixes)")
+    print("1. Dump an ASN (peers or prefixes)")
     print("2. Find prefixes from ASN dump (json peers file)")
-    print("3. Generate iptables script from ASN dump (json peers dump)")
+    print("3. Generate iptables script from ASN dump (json prefixes dump)")
     return input("Enter your choice: ").strip()
 
 # Function to read a JSON file by name
@@ -33,12 +33,12 @@ def generate_iptables_script(data, script_filename):
             for item in data:
                 if 'prefix' in item:
                     prefix = item['prefix']
+                    # IPv6 prefix
                     if ':' in prefix:
-                        # IPv6 prefix
                         script_file.write(f"ip6tables -A INPUT -s {prefix} -j DROP\n")
                         script_file.write(f"ip6tables -A OUTPUT -d {prefix} -j DROP\n")
+                    # IPv4 prefix
                     else:
-                        # IPv4 prefix
                         script_file.write(f"iptables -A INPUT -s {prefix} -j DROP\n")
                         script_file.write(f"iptables -A OUTPUT -d {prefix} -j DROP\n")
                 else:
@@ -52,6 +52,29 @@ def main():
     # Get the user's choice
     choice = get_user_choice()
 
+    # Set up Chrome options
+    options = Options()
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument('--headless')
+
+    # Ask for the host OS 
+    os_type = input("What OS are you using? \n1. Debian or 2) Arch: ")
+
+    # Set the binary based on host OS
+    if os_type == "1":
+        options.binary_location = "/usr/bin/chromium-browser"
+    elif os_type == "2":
+        options.binary_location = "/usr/sbin/chromium"
+
+    if debug:
+        options.add_argument("--remote-debugging-port=9222")
+
+    driver = webdriver.Chrome(options=options)
+
     if choice in ["1", "2", "3"]:
         options = Options()
         options.add_argument("start-maximized")
@@ -60,125 +83,187 @@ def main():
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
         options.add_argument('--headless')
-
-        # Ask for the host OS.
-        os_type = input("What OS are you using? \n1. Debian or 2) Arch: ")
-
-        # Set the binary based on host OS
-        if os_type == "1":
-            options.binary_location = "/usr/bin/chromium-browser"
-        elif os_type == "2":
-            options.binary_location = "/usr/sbin/chromium"
+        options.binary_location = "/usr/bin/chromium-browser"
 
         if debug:
             options.add_argument("--remote-debugging-port=9222")
 
         driver = webdriver.Chrome(options=options)
 
-        if choice in ["1", "2"]:
-            asn = input("Enter the ASN: ").strip()
+    if choice == "1":
+        asn = input("Enter the ASN: ").strip()
 
-            if not asn.isdigit():
-                print("Invalid ASN.")
-                exit()
+        if not asn.isdigit():
+            print("Invalid ASN.")
+            exit()
 
-        if choice == "1":
-            shouldSort = input("Sort by country? (y/n): ").strip().lower() == "y"
-            whatSort = input("What country? (e.g. US): ").strip() if shouldSort else None
-            shouldDump = input("Dump results to .json file? (y/n): ").strip().lower() == "y"
-            dumps = []
+        dumps = []
+        shouldDump = False
+        shouldSort = False
+        whatSort = ""
 
-            # Print the options
-            print("\n1. Peers")
-            print("2. Prefixes")
-            lookup_choice = input("Enter your choice: ")
+        # Should we sort by flag?
+        sort = input("Sort by country? (y/n): ").strip()
 
-            # Peers
-            if lookup_choice == "1":
-                url = f'https://bgp.tools/as/{asn}#connectivity'
-                try:
-                    # Get the page data by xpath
-                    driver.get(url)
-                    xpath = '/html/body/div[1]/div[5]/div[2]/div[1]/div[7]/table[3]/tbody'
-                    tbody = driver.find_element(By.XPATH, xpath)
+        if sort.lower() == "y":
+            shouldSort = True
 
-                    if tbody is not None:
-                        for tr in tbody.find_elements(By.TAG_NAME, 'tr'):
-                            flag, asn, asn_name = "", "", ""
-                            trs = tr.find_elements(By.TAG_NAME, 'td')
-                            img = trs[0].find_element(By.TAG_NAME, 'img')
-                            flag = img.get_attribute('title')
-                            asn = trs[1].get_attribute('data-sort')
-                            asn_name = trs[2].text
+            # Ask the user what country they want to sort by
+            whatSort = input("What country? (e.g. US): ").strip()
 
-                            # Add to the dumps list if we should dump
-                            if shouldDump and (not shouldSort or flag == whatSort):
-                                dumps.append({"flag": flag, "asn": asn, "asn_name": asn_name})
+        # Ask the user if they want to dump the results to a .json file
+        dump = input("Dump results to .json file? (y/n): ")
 
-                            # Print the results if we should sort or if the flag matches
-                            if not shouldSort or flag == whatSort:
-                                print(f'{flag} {asn} {asn_name}')
-                                
-                        # Dump the results to a file if we should
-                        if shouldDump:
-                            with open('peers.json', 'w') as f:
-                                json.dump(dumps, f)
-                except Exception as e:
-                    print(f"Error while processing: {str(e)}")
+        if dump.lower() == "y":
+            shouldDump = True
 
-            # Prefixes
-            elif lookup_choice == "2":
-                url = f'https://bgp.tools/as/{asn}#prefixes'
-                try:
-                    # Get the page data by css selector
-                    driver.get(url)
-                    css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
-                    elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
+        # Does the user want to see their peers or their prefixes?
+        print("\n1. Peers")
+        print("2. Prefixes")
+        lookup_choice = input("Enter your choice: ")
 
-                    # Dump the results to a file if we should
-                    dumps = [{"prefix": element.text} for element in elements] if shouldDump else None
-                    prefixes = [element.text for element in elements]
+        if lookup_choice == "1":
+            # Navigate to the URL
+            url = f'https://bgp.tools/as/{asn}#connectivity'
+            driver.get(url)
 
-                    # Print the results
-                    for prefix in prefixes:
-                        print(prefix)
+            # Find the tbody element using XPath
+            xpath = '/html/body/div[1]/div[5]/div[2]/div[1]/div[7]/table[3]/tbody'
+            tbody = driver.find_element(By.XPATH, xpath)
 
-                    # Dump the results to a file if we should
+            # Check if tbody is found
+            if tbody is not None:
+                # Iterate through each tr inside tbody
+                for tr in tbody.find_elements(By.TAG_NAME, 'tr'):
+                    flag = ""
+                    asn = ""
+                    asn_name = ""
+
+                    trs = tr.find_elements(By.TAG_NAME, 'td')
+
+                    # Find the td element inside each tr
+                    td1 = trs[0]
+                    if td1 is not None:
+                        # Get the image element inside the td
+                        img = td1.find_element(By.TAG_NAME, 'img')
+
+                        # Set the flag
+                        flag = img.get_attribute('title')
+
+                    # Remove the first td element
+                    as_td = trs[1]
+                    if as_td is not None:
+                        # ASN is stored inside attribute "data-sort"
+                        asn = as_td.get_attribute('data-sort')
+
+                    # Get the asn_name
+                    asn_name_td = trs[2]
+                    if asn_name_td is not None:
+                        asn_name = asn_name_td.text
+
+                    # Append the data to the list
                     if shouldDump:
-                        with open('prefixes.json', 'w') as f:
-                            json.dump(dumps, f)
-                except Exception as e:
-                    print(f"Error while processing: {str(e)}")
+                        if shouldSort:
+                            if flag == whatSort:
+                                dumps.append({
+                                    "flag": flag,
+                                    "asn": asn,
+                                    "asn_name": asn_name
+                                })
+                        else:  # dont sort
+                            dumps.append({
+                                "flag": flag,
+                                "asn": asn,
+                                "asn_name": asn_name
+                            })
 
-        # Find prefixes from ASN dump
-        if choice == "2":
-            filename = input("Enter the filename: ").strip()
-            data = read_json_file(filename)
-            if data is not None:
+                    # Print the data
+                    if shouldSort:
+                        if flag == whatSort:
+                            print(f'{flag} {asn} {asn_name}')
+                    else:  # dont sort
+                        print(f'{flag} {asn} {asn_name}')
+
+            else:
+                print("Tbody not found.")
+
+            if shouldDump:
+                with open('peers.json', 'w') as f:
+                    json.dump(dumps, f)
+
+        if lookup_choice == "2":
+            # Navigate to the URL
+            url = f'https://bgp.tools/as/{asn}#prefixes'
+            driver.get(url)
+
+            # im sorry : (
+            css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
+            elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
+
+            # Iterate through the elements to get the text content
+            for element in elements:
+                prefix_text = element.text
+                print(prefix_text)
+
+                if shouldDump:
+                    dumps.append({
+                        "prefix": prefix_text
+                    })
+
+            if shouldDump:
+                with open('prefixes.json', 'w') as f:
+                    json.dump(dumps, f)
+                    
+    if choice == "2":
+        filename = input("Enter the filename: ").strip()
+        data = read_json_file(filename)
+        if data is not None:
                 # Get the prefixes for each ASN based on output
-                output_filename = input("Enter the output filename: ").strip()
+            output_filename = input("Enter the output filename: ").strip()
+            with open(output_filename, 'w') as f:
+                f.write("Prefix\n")
+                # Loop through the data
+                for item in data:
+                    url = f'https://bgp.tools/as/{item["asn"]}#prefixes'
+                    try:
+                        # Get the page data by css selector
+                        driver.get(url)
+                        css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
+                        elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
+                        prefixes = [element.text for element in elements]
+                        f.write("\n".join(prefixes) + "\n")
+                    except Exception as e:
+                        print(f"Error while processing: {str(e)}")
 
-                with open(output_filename, 'w') as f:
-                    f.write("Prefix\n")
-                    # Loop through the data
-                    for item in data:
-                        url = f'https://bgp.tools/as/{item["asn"]}#prefixes'
-                        try:
-                            # Get the page data by css selector
-                            driver.get(url)
-                            css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
-                            elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
-                            prefixes = [element.text for element in elements]
-                            f.write("\n".join(prefixes) + "\n")
-                        except Exception as e:
-                            print(f"Error while processing: {str(e)}")
+    if choice == "3":
+        # Get the filename
+        filename = input("Enter the filename: ").strip()
+        data = read_json_file(filename)
+        if data is not None:
+            # Get the prefixes for each ASN based on output
+            output_filename = input("Enter the output filename: ").strip()
 
-        # Generate iptables script from ASN dump
-        if choice == "3":
-            filename = input("Enter the filename for the JSON peers file: ").strip()
-            data = read_json_file(filename)
-            if data is not None:
-                generate_iptables_script(data, 'iptables_rules.sh')
+            with open(output_filename, 'w') as f:
+                f.write("Prefix\n")
+                # Loop through the data
+                for item in data:
+                    url = f'https://bgp.tools/as/{item["asn"]}#prefixes'
+                    try:
+                        # Get the page data by css selector
+                        driver.get(url)
+                        css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
+                        elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
+                        prefixes = [element.text for element in elements]
+                        f.write("\n".join(prefixes) + "\n")
+                    except Exception as e:
+                        print(f"Error while processing: {str(e)}")
+
+    # Generate iptables script from ASN dump
+    if choice == "3":
+        filename = input("Enter the filename for the JSON prefixes file: ").strip()
+        data = read_json_file(filename)
+        if data is not None:
+            generate_iptables_script(data, 'iptables_rules.sh')
 
     if choice not in ["1", "2", "3"]:
         print("Invalid choice. Exiting...")
