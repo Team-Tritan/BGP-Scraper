c8f873c0ca3562f1d0280dbdd35a994a95d94cbc
Dylan <dylan@tritan.dev>
2023-10-05 20:17:17 -0400

    added error handling and handling for different chromium paths based on os

--- scraper.py
+++ scraper.py
@@ -8,29 +8,42 @@ debug = False
 def get_user_choice():
     print("1. Convert .json to .csv (peers or prefixes)")
     print("2. Lookup an ASN (peers or prefixes)")
-    print("3. Lookup prefixes by ASN dump (json peers file)")
-    print ("4. Generate iptables script from json peers file")
+    print("3. Find prefixes from ASN dump (json peers file)")
+    print("4. Generate iptables script script ASN dump")
     return input("Enter your choice: ").strip()
 
 def read_json_file(filename):
-    with open(filename, 'r') as f:
-        return json.load(f)
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON file: {str(e)}")
+        return None
 
 def write_csv_file(output_filename, data, headers):
-    with open(output_filename, 'w') as f:
-        f.write(",".join(headers) + "\n")
-        for item in data:
-            f.write(",".join([str(item[field]) for field in headers]) + "\n")
+    try:
+        with open(output_filename, 'w') as f:
+            f.write(",".join(headers) + "\n")
+            for item in data:
+                f.write(",".join([str(item[field]) for field in headers]) + "\n")
+    except Exception as e:
+        print(f"Error writing to {output_filename}: {str(e)}")
 
 def generate_iptables_script(data, script_filename):
-    with open(script_filename, 'w') as script_file:
-        script_file.write("#!/bin/bash\n\n")
-        for item in data:
-            if 'ip' in item:
-                ip_address = item['ip']
-                script_file.write(f"iptables -A INPUT -s {ip_address} -j DROP\n")
-                script_file.write(f"iptables -A OUTPUT -d {ip_address} -j DROP\n")
-        print(f"iptables rules written to {script_filename}")
+    try:
+        with open(script_filename, 'w') as script_file:
+            script_file.write("#!/bin/bash\n\n")
+            for item in data:
+                if 'ip' in item:
+                    ip_address = item['ip']
+                    script_file.write(f"iptables -A INPUT -s {ip_address} -j DROP\n")
+                    script_file.write(f"iptables -A OUTPUT -d {ip_address} -j DROP\n")
+        print(f"Iptables rules written to {script_filename}")
+    except Exception as e:
+        print(f"Error generating iptables script: {str(e)}")
 
 def main():
     choice = get_user_choice()
@@ -38,15 +51,16 @@ def main():
     if choice == "1":
         filename = input("Enter the filename: ").strip()
         data = read_json_file(filename)
-        output_filename = input("Enter the output filename: ").strip()
+        if data is not None:
+            output_filename = input("Enter the output filename: ").strip()
 
-        if "asn" in data[0]:
-            headers = ["Flag", "ASN", "ASN Name"]
-            write_csv_file(output_filename, data, headers)
+            if "asn" in data[0]:
+                headers = ["Flag", "ASN", "ASN Name"]
+                write_csv_file(output_filename, data, headers)
 
-        if "prefix" in data[0]:
-            headers = ["Prefix"]
-            write_csv_file(output_filename, data, headers)
+            if "prefix" in data[0]:
+                headers = ["Prefix"]
+                write_csv_file(output_filename, data, headers)
 
     if choice == "2":
         options = Options()
@@ -56,7 +70,14 @@ def main():
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
         options.add_argument('--headless')
-        options.binary_location = "/usr/bin/chromium-browser"
+
+        # What is the host os?
+        os_type = input("What OS are you using? 1) Debian or 2) Arch:  ")
+
+        if os_type == "1":
+            options.binary_location = "/usr/bin/chromium-browser"
+        elif os_type == "2":
+            options.binary_location = "/usr/sbin/chromium"
 
         if debug:
             options.add_argument("--remote-debugging-port=9222")
@@ -80,45 +101,50 @@ def main():
 
         if choice == "1":
             url = f'https://bgp.tools/as/{asn}#connectivity'
-            driver.get(url)
+            try:
+                driver.get(url)
+                xpath = '/html/body/div[1]/div[5]/div[2]/div[1]/div[7]/table[3]/tbody'
+                tbody = driver.find_element(By.XPATH, xpath)
 
-            xpath = '/html/body/div[1]/div[5]/div[2]/div[1]/div[7]/table[3]/tbody'
-            tbody = driver.find_element(By.XPATH, xpath)
+                if tbody is not None:
+                    for tr in tbody.find_elements(By.TAG_NAME, 'tr'):
+                        flag, asn, asn_name = "", "", ""
+                        trs = tr.find_elements(By.TAG_NAME, 'td')
+                        img = trs[0].find_element(By.TAG_NAME, 'img')
+                        flag = img.get_attribute('title')
+                        asn = trs[1].get_attribute('data-sort')
+                        asn_name = trs[2].text
 
-            if tbody is not None:
-                for tr in tbody.find_elements(By.TAG_NAME, 'tr'):
-                    flag, asn, asn_name = "", "", ""
-                    trs = tr.find_elements(By.TAG_NAME, 'td')
-                    img = trs[0].find_element(By.TAG_NAME, 'img')
-                    flag = img.get_attribute('title')
-                    asn = trs[1].get_attribute('data-sort')
-                    asn_name = trs[2].text
+                        if shouldDump and (not shouldSort or flag == whatSort):
+                            dumps.append({"flag": flag, "asn": asn, "asn_name": asn_name})
 
-                    if shouldDump and (not shouldSort or flag == whatSort):
-                        dumps.append({"flag": flag, "asn": asn, "asn_name": asn_name})
+                        if not shouldSort or flag == whatSort:
+                            print(f'{flag} {asn} {asn_name}')
 
-                    if not shouldSort or flag == whatSort:
-                        print(f'{flag} {asn} {asn_name}')
-
-                if shouldDump:
-                    with open('peers.json', 'w') as f:
-                        json.dump(dumps, f)
+                    if shouldDump:
+                        with open('peers.json', 'w') as f:
+                            json.dump(dumps, f)
+            except Exception as e:
+                print(f"Error while processing: {str(e)}")
 
         if choice == "2":
             url = f'https://bgp.tools/as/{asn}#prefixes'
-            driver.get(url)
-            css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
-            elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
+            try:
+                driver.get(url)
+                css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
+                elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
 
-            dumps = [{"prefix": element.text} for element in elements] if shouldDump else None
-            prefixes = [element.text for element in elements]
+                dumps = [{"prefix": element.text} for element in elements] if shouldDump else None
+                prefixes = [element.text for element in elements]
 
-            for prefix in prefixes:
-                print(prefix)
+                for prefix in prefixes:
+                    print(prefix)
 
-            if shouldDump:
-                with open('prefixes.json', 'w') as f:
-                    json.dump(dumps, f)
+                if shouldDump:
+                    with open('prefixes.json', 'w') as f:
+                        json.dump(dumps, f)
+            except Exception as e:
+                print(f"Error while processing: {str(e)}")
 
     if choice == "3":
         options = Options()
@@ -136,22 +162,29 @@ def main():
         driver = webdriver.Chrome(options=options)
         filename = input("Enter the filename: ").strip()
         data = read_json_file(filename)
-        output_filename = input("Enter the output filename: ").strip()
+        if data is not None:
+            output_filename = input("Enter the output filename: ").strip()
 
-        with open(output_filename, 'w') as f:
-            f.write("Prefix\n")
-            for item in data:
-                url = f'https://bgp.tools/as/{item["asn"]}#prefixes'
-                driver.get(url)
-                css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
-                elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
-                prefixes = [element.text for element in elements]
-                f.write("\n".join(prefixes) + "\n")
+            with open(output_filename, 'w') as f:
+                f.write("Prefix\n")
+                for item in data:
+                    url = f'https://bgp.tools/as/{item["asn"]}#prefixes'
+                    try:
+                        driver.get(url)
+                        css_selector = '#donotscrapebgptools-prefixlist-tbody td:nth-child(2) a'
+                        elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
+                        prefixes = [element.text for element in elements]
+                        f.write("\n".join(prefixes) + "\n")
+                    except Exception as e:
+                        print(f"Error while processing: {str(e)}")
 
         driver.quit()
 
     if choice == "4":
-        generate_iptables_script(dumps, 'iptables_rules.sh')
+        filename = input("Enter the filename for the JSON peers file: ").strip()
+        data = read_json_file(filename)
+        if data is not None:
+            generate_iptables_script(data, 'iptables_rules.sh')
 
 if __name__ == "__main__":
     try:
